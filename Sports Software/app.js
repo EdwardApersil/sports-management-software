2c2b74ab281a1f61200f3244306f38f9b23c426b
Edward Apersil <64957496+EdwardApersil@users.noreply.github.com>
2022-07-19 10:03:12 +0000

    Added comments to the code

--- Sports Software/app.js
+++ Sports Software/app.js
@@ -1,12 +1,20 @@
-let raceNumber = Math.floor(Math.random() * 1000);
+
+//Taking users input
       let runnerRegistered = true;
       let runnersName = prompt("please enter your name: ");
       let runnersAge = parseInt(prompt("Please enter your age: "));
+//Converting users input into random numbers
+      let raceNumber = Math.floor(Math.random() * 1000);
 
+//This piece of code authomatically generate users ID number according to their inputs
       if (runnerRegistered && runnersAge === 18) {
         raceNumber * 1000;
       }
-      //racing conditional flow
+
+
+      /*racing conditional flow checks users 
+      input and then deplay they're name, time to race,
+       and thier respective user ID */
       if (runnerRegistered && runnersAge > 18) {
         document.write(
           `${runnersName}, Your race starts at 9:00AM. And your race ID is: ${raceNumber}`
